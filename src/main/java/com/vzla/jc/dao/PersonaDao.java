b24f2dc0939db9e83d94cbe818eeea656afd51e6
COSSIGOMEZ <COSSIGOMEZ@192.168.0.33>
2021-04-04 08:37:44 -0300

    Correccion en la importacion de librerias

--- src/main/java/com/vzla/jc/dao/PersonaDao.java
+++ src/main/java/com/vzla/jc/dao/PersonaDao.java
@@ -2,9 +2,10 @@
 package com.vzla.jc.dao;
 
 import com.vzla.jc.domain.Persona;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface PersonaDao extends CrudRepository<Persona, Long>{
+
+public interface PersonaDao extends JpaRepository<Persona, Long>{
     
 }
